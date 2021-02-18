7753ffa966662d0ceb82e0fe5a15127d984c3d79
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-02-19 01:38:46 +0700

    Create styling home vision-mission, trends, products

--- application/views/home.php
+++ application/views/home.php
@@ -1,2 +1,16 @@
-<p>home works</p>
-<p><i class="fas fa-user"></i> <!-- uses solid style --></p>
+<main>
+	<div class="container">
+		<section class="home-banner custom-my">
+			<div class="row">
+                <div class="col-lg-5 col-md-7">
+                    <h1 class="banner-title">VIRTUDROBE</h1>
+                    <p class="banner-text">The first Virtual Wardrobe in Indonesia. Start using our technology to reflect your lifestyle.</p>
+                    <a class="btn c-btn-primary" href="#">Discover Now</a>
+                </div>
+                <div class="col-lg-6 offset-lg-1">
+                    <img class="img-fluid" src="<?= base_url('assets/'); ?>img/home-vector.png" alt="">
+                </div>
+            </div>
+		</section>
+	</div>
+</main>
