54e0f7308af4f6aedb0e1ccb2536441bce2cd2f7
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-02-18 23:56:12 +0700

    Add styling and content to header and footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,57 @@
+    <footer class="footer custom-py">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <img class="footer-brand" src="<?= base_url('assets/'); ?>img/logo-white.png" alt="Logo Virtudrobe White">
+                    <p>
+                        &copy;2021 Virtudrobe All Rights Reserved.
+                    </p>
+                    <ul class="list-inline social-media">
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <img src="<?= base_url('assets/'); ?>icon/instagram.png" alt="Instagram">
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <img src="<?= base_url('assets/'); ?>icon/twitter.png" alt="Twitter">
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <img src="<?= base_url('assets/'); ?>icon/globe.png" alt="Internet">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4">
+                    <h6 class="title">Explorer</h6>
+                    <ul class="useful-links list-unstyled">
+                        <li>About Us</li>
+                        <li>All Features</li>
+                        <li>Our Team</li>
+                        <li>Our Services</li>
+                        <li>Latest Blog</li>
+                        <li>Help</li>
+                        <li>Support</li>
+                        <li>Contact Us</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms & Condition</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 offset-lg-2">
+                    <h6 class="title">Contact Us</h6>
+                    <ul class="list-unstyled">
+                        <li>help@virtudrobe.com</li>
+                        <li>Phone +6277814045</li>
+                        <li>Fax +6277814045</li>
+                        <li>Mon - Fri  9am-5pm</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
     <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery-3.5.1.min.js"></script>
     <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="<?= base_url('assets/'); ?>vendor/fontawesome/js/all.js"></script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,46 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Virtudrobe</title>
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/fontawesome/css/all.min.css">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Virtudrobe</title>
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/fontawesome/css/all.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>css/styles.css">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#"><img class="img-fluid" src="<?= base_url('assets/'); ?>img/logo.png"
+					alt="Logo Virtudrobe"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+
+          <!-- Home -->
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+
+          <!-- Product -->
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Product
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Category 1</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Category 2</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Category 3</a>
+						</div>
+					</li>
+          
+          <!-- Wardrobe -->
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							Wardrobe
+						</a>
+					</li>
+
+					<!-- Cart Dropdown-->
+					<li class="nav-item dropdown">
+						<a class="nav-link icon-nav cart-dropdown dropdown-toggle" href="javascript:void(0)" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fas fa-shopping-cart fa-2x py-1 px-1"></i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							cart content
+						</div>
+					</li>
+
+					<!-- User Dropdown-->
+					<li class="nav-item dropdown">
+						<a class="nav-link icon-nav dropdown-toggle" href="javascript:void(0)" id="navbarDropdown" role="button"
+							data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fas fa-user-circle fa-3x "></i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Login</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Sign Up</a>
+						</div>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+	</nav>
